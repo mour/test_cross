2d6a9347b74e61147599396a77a6304f04e300f6
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2021-12-26 17:09:54 +0100

    Added a dependency & some test output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate local_ip_address;
+
 fn main() {
     println!("Hello, world!");
+
+    println!("My IP is: {}", local_ip_address::local_ip().unwrap());
+
+    println!();
+
+    println!("All IPs associated with this computer:");
+    for (name, ip) in local_ip_address::list_afinet_netifas().unwrap() {
+        println!("{}:\t{:?}", name, ip);
+    }
 }
